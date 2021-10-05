f05af832e767f4b3f7e862b02452472e60374032
musiciancoder <andresguitarra1980@gmail.com>
2021-10-05 18:32:20 -0300

    balanceo con restTemplate

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,10 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoSevice;
 public class ProductoController {
 
     @Autowired
-    private Environment env; //La clase envireonment permite acceder a las propiedades del archivo application.properties; sirve para mas abajo obtener el puerto
+    private Environment env; //La clase envireonment permite acceder a las propiedades del archivo application.properties al usar feign en el servicio que lo invoca; sirve para mas abajo obtener el puerto
+
+    @Value("${server.port}") //se invoca con RestTemplate en el otro servicio
+    private Integer port;
 
     @Autowired
     private IProductoSevice productoService;
@@ -24,7 +28,8 @@ public class ProductoController {
     @GetMapping("/listar")
     public List<Producto> listar() {
         return productoService.findAll().stream().map(producto -> {
-            producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+          //  producto.setPort(Integer.parseInt(env.getProperty("local.server.port"))); //esta linea se usa con Feign
+            producto.setPort(port); //con restTemplate
             return producto;
         }).collect(Collectors.toList());
     }
@@ -32,7 +37,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
         Producto producto = productoService.findById(id);
-        producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+     //   producto.setPort(Integer.parseInt(env.getProperty("local.server.port"))); //esta linea se usa con Feign
+        producto.setPort(port); //con restTemplate
         return producto;
     }
 
